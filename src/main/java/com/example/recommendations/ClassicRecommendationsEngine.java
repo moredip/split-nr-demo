075e696d50e54da59b0a09f8c15aaded19c42010
Pete Hodgson <git@thepete.net>
2020-03-27 11:35:52 -0700

    simulate expensive computation in the recommendation engines

--- src/main/java/com/example/recommendations/ClassicRecommendationsEngine.java
+++ src/main/java/com/example/recommendations/ClassicRecommendationsEngine.java
@@ -11,6 +11,14 @@ import static com.example.recommendations.domain.Product.generateFakeProduct;
 public class ClassicRecommendationsEngine implements ProductRecommender {
     @Override
     public Stream<Product> productsRelatedTo(Product product){
+        sleep(100); //simulate a short computation delay
         return Stream.generate(Product::generateFakeProduct).limit(10);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis); //simulate an expensive computation delay
+        } catch (InterruptedException e) {
+        }
     }
 }
--- src/main/java/com/example/recommendations/NextGenRecommendationsEngine.java
+++ src/main/java/com/example/recommendations/NextGenRecommendationsEngine.java
@@ -8,6 +8,15 @@ import java.util.stream.Stream;
 public class NextGenRecommendationsEngine implements ProductRecommender {
     @Override
     public Stream<Product> productsRelatedTo(Product product) {
+        sleep(600);
+
         return Stream.generate(Product::generateFakeProduct).limit(20);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis); //simulate an expensive computation delay
+        } catch (InterruptedException e) {
+        }
     }
 }
